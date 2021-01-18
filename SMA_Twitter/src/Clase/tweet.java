596f20b8adbc22aea2a9009c78bcc3718a52e879
jbromero3 <jbromero3@utpl.edu.ec>
2021-01-18 10:54:12 -0500

    Eliminacion de codigo que no se usara por el momento

--- SMA_Twitter/src/Clase/tweet.java
+++ SMA_Twitter/src/Clase/tweet.java
@@ -4,30 +4,15 @@ package Clase;
  * @author T O S H I B A
  */
 public class tweet {
-    private int id;
     private String tweet;
     private String mensaje;
 
     public tweet() {
     }
-
-    public tweet(int id, String tweet, String mensaje) {
-        this.id = id;
-        this.tweet = tweet;
-        this.mensaje = mensaje;
-    }
     
     public tweet(String tweet, String mensaje) {
         this.tweet = tweet;
         this.mensaje = mensaje;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getTweet() {
@@ -48,6 +33,6 @@ public class tweet {
 
     @Override
     public String toString() {
-        return "tweet{" + "id=" + id + ", tweet=" + tweet + ", mensaje=" + mensaje + '}';
+        return "tweet{" + "tweet=" + tweet + ", mensaje=" + mensaje + '}';
     }
 }
--- SMA_Twitter/src/SMA_Twitter/Agente_Recopilador.java
+++ SMA_Twitter/src/SMA_Twitter/Agente_Recopilador.java
@@ -1,6 +1,5 @@
 package SMA_Twitter;
 
-import BL.BLRecopilador;
 import Clase.tweet;
 import DAT.LeerArchivo;
 import jade.core.Agent;
@@ -18,15 +17,7 @@ public class Agente_Recopilador extends Agent{
     
     String path = "DataSet/smileannotationsfinal.csv";
     LeerArchivo leer = new LeerArchivo(path);
-    BLRecopilador objBLRecopilador = new BLRecopilador();
     ArrayList<tweet> lista;
-    String obj;
-    String[] arr;
-    String aux1;
-    String aux2;
-    
-        
-    tweet data = new tweet();
     
     public void setup(){
         
@@ -37,47 +28,7 @@ public class Agente_Recopilador extends Agent{
             System.out.println(lista.get(i));
         }
         
-        /*System.out.println(lista.get(0));
-        obj = lista.get(0);
-        arr = obj.split(",");
-        aux1 = arr[0];
-        aux2 = arr[1];
-        System.out.printf("ID: %s - Mensaje: %s",aux1,aux2);        
-        
-        try {
-                tweet objData = new tweet(0,aux1, aux2);
-                objBLRecopilador.AgregarFraseDB(objData);
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(Agente_Recopilador.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SQLException ex) {
-            Logger.getLogger(Agente_Recopilador.class.getName()).log(Level.SEVERE, null, ex);
-        }*/
-        
-        //Scanner datos = new Scanner(System.in);
-        /*int numero1 = 0;
-        int numero2 = 0;*/
-        /*ArrayList<String> Feliz = new ArrayList<String>();
-        
-        System.out.print("Cantidad de pablabras a buscar: ");
-        int tamaño = datos.nextInt();
-        for (int i = 0; i <= tamaño; i++) {
-            System.out.println("Palabra:");
-            Feliz.add(datos.nextLine());
-        }
-        
-        for (int i = 0; i <= tamaño; i++) {
-            System.out.println(Feliz.get(i));
-        }*/
-        
-        /*System.out.print("Ingrese el trabajo a realizar (suma, resta, multiplicacion, divicion): ");
-        String trabajo = datos.nextLine();
-        System.out.print("Operacion: "+trabajo+"\n");
-        System.out.print("Ingrese el primer numero: ");
-        numero1 = datos.nextInt();
-        System.out.print("Ingrese el segundo numero: ");
-        numero2 = datos.nextInt();
-        
-        //doDelete();*/
+        //doDelete();
     }
     
     /*protected void takeDown(){
