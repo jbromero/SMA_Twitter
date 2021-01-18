3b22e191fcdaf5fe25b2e30cfdc719cda21daa14
jbromero3 <jbromero3@utpl.edu.ec>
2021-01-18 12:50:37 -0500

    Se agrego el clase main para probar el agente

---
+++ SMA_Twitter/src/Principal/Prueba.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Principal;
+
+import SMA_Twitter.Agente_Recopilador;
+
+/**
+ *
+ * @author T O S H I B A
+ */
+public class Prueba {
+    public static void main(String[] args) {
+        Agente_Recopilador obj = new Agente_Recopilador();
+        
+        obj.setup();
+    }
+}
